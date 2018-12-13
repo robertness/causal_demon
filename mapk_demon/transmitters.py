6084917ca6350f76a72f9e8e624b510a441aa2c9
kaushalpaneri <paneri.k@husky.neu.edu>
2018-12-13 14:00:50 -0500

    Inference Done

--- mapk_demon/transmitters.py
+++ mapk_demon/transmitters.py
@@ -2,8 +2,9 @@ from __future__ import division
 
 from functools import partial
 
+import pyro
 from pyro import sample
-from pyro.distributions import LogNormal
+from pyro.distributions import Normal, Uniform
 from torch import tensor
 
 
@@ -12,14 +13,14 @@ def g1(a):
 
 
 def g2(a):
-    return a**2 / (a**2 + a + 1)
+    return a ** 2 / (a ** 2 + a + 1)
 
 
 def f(mu, N):
-    return N ** (0.5) * mu + mu
+    return N * mu ** (0.5) + mu
 
 
-constants = {
+hyperparameters = {
     "t_3k": 1.2,
     "t_2k": 1.2,
     "t_k": 0.003
@@ -36,7 +37,7 @@ params = {
 
 
 def f_map3k(E1, N_3k):
-    total_3k = constants['t_3k']
+    total_3k = hyperparameters['t_3k']
     alpha_3k = params['alpha_3k']
     nu_3k = params['nu_3k']
 
@@ -46,7 +47,7 @@ def f_map3k(E1, N_3k):
 
 
 def f_map2k(map3k, N_2k):
-    total_2k = constants['t_2k']
+    total_2k = hyperparameters['t_2k']
     alpha_2k = params['alpha_2k']
     nu_2k = params['nu_2k']
 
@@ -56,7 +57,7 @@ def f_map2k(map3k, N_2k):
 
 
 def f_mapk(map2k, N_k):
-    total_k = constants['t_k']
+    total_k = hyperparameters['t_k']
     alpha_k = params['alpha_k']
     nu_k = params['nu_k']
 
@@ -67,10 +68,11 @@ def f_mapk(map2k, N_k):
 
 def mapk_signaling(noise_dists):
 
-    E1 = 1.
-    N_3k = sample('N_3k', noise_dists['N_3k'])
-    N_2k = sample('N_2k', noise_dists['N_2k'])
-    N_k = sample('N_k', noise_dists['N_k'])
+    with pyro.iarange("model"):
+        E1 = sample('E1', Uniform(1.5e-5, 6e-5))
+        N_3k = sample('N_3k', noise_dists['N_3k'])
+        N_2k = sample('N_2k', noise_dists['N_2k'])
+        N_k = sample('N_k', noise_dists['N_k'])
 
     map3k = f_map3k(E1, N_3k)
     map2k = f_map2k(map3k,N_2k)
@@ -82,11 +84,10 @@ def mapk_signaling(noise_dists):
         'mapk': mapk
     }
 
-
 if __name__ == "__main__":
 
     # Noise Distributions
     noise_vars = ['N_3k', 'N_2k', 'N_k']
-    noise_prior = {N: LogNormal(0, 10) for N in noise_vars}
+    noise_prior = {N: Normal(0, 1) for N in noise_vars}
 
     print(mapk_signaling(noise_dists=noise_prior))
