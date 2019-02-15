2d9410f1896e7df19ba67e5a2ef7a90270132a84
kaushalpaneri <paneri.k@husky.neu.edu>
2019-02-15 13:48:35 -0500

    mapk inference working

---
+++ mapk_demon/svi_test.py
@@ -0,0 +1,11 @@
+'''
+svi_test
+Author: Kaushal Paneri
+Project: causal_demon
+Date of Creation: 1/2/19
+'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
---
+++ run_mapk.py
@@ -0,0 +1,11 @@
+'''
+run_mapk
+Author: Kaushal Paneri
+Project: causal_demon
+Date of Creation: 12/29/18
+'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
