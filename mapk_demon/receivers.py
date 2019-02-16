e22069a8b988c5d70636b8c99d4e8a5858270011
kaushalpaneri <paneri.k@husky.neu.edu>
2019-02-16 14:21:17 -0500

    counterfactual on companion model

--- mapk_demon/receivers.py
+++ mapk_demon/receivers.py
@@ -4,7 +4,7 @@ from functools import partial
 
 import pyro
 from pyro import sample
-from pyro.distributions import Normal, Uniform
+from pyro.distributions import Normal, Uniform, Delta
 
 import pyro.optim
 import torch
@@ -29,39 +29,47 @@ hyperparameters = {
 }
 
 
-def f_map3k(E1, N_3k, params):
+def f_map3k(E1, N_3k, params, mode):
     total_3k = hyperparameters['t_3k']
     alpha_3k = params['alpha_3k'].rsample()
     nu_3k = params['nu_3k'].rsample()
 
     map3k_mu = total_3k * g1(E1 * (alpha_3k/nu_3k))
 
-    map3k = sample("map3k", Normal(f(map3k_mu, N_3k), 1.))
-    return map3k
+    if mode == 'companion':
+        return sample("map3k", Normal(f(map3k_mu, N_3k), 1.))
+    else:
+        return sample("map3k", Delta(f(map3k_mu, N_3k)))
 
 
-def f_map2k(map3k, N_2k, params):
+def f_map2k(map3k, N_2k, params, mode):
     total_2k = hyperparameters['t_2k']
     alpha_2k = params['alpha_2k'].rsample()
     nu_2k = params['nu_2k'].rsample()
 
     map2k_mu = total_2k * g2(map3k * (alpha_2k / nu_2k))
 
-    map2k = sample("map2k", Normal(f(map2k_mu, N_2k), 1.))
-    return map2k
+    if mode == 'companion':
+        return sample("map2k", Normal(f(map2k_mu, N_2k), 1.))
+    else:
+        return sample("map2k", Delta(f(map2k_mu, N_2k)))
 
 
-def f_mapk(map2k, N_k, params):
+def f_mapk(map2k, N_k, params, mode):
     total_k = hyperparameters['t_k']
     alpha_k = params['alpha_k'].rsample()
     nu_k = params['nu_k'].rsample()
 
     mapk_mu = total_k * g2(map2k * (alpha_k / nu_k))
-    mapk = sample("mapk", Normal(f(mapk_mu, N_k), 1.))
-    return mapk
+
+    if mode == 'companion':
+        return sample("mapk", Normal(f(mapk_mu, N_k), 1.))
+    else:
+        return sample("mapk", Delta(f(mapk_mu, N_k)))
 
 
-def mapk_receiver(noise_dists):
+def mapk_companion(noise_dists):
+    mode = 'companion'
     # Parameters
     al_m = torch.tensor(700.)
     al_s = torch.tensor(1.)
@@ -84,12 +92,78 @@ def mapk_receiver(noise_dists):
         N_2k = sample('N_2k', noise_dists['N_2k'])
         N_k = sample('N_k', noise_dists['N_k'])
 
-    map3k = f_map3k(E1, N_3k, params)
-    map2k = f_map2k(map3k,N_2k, params)
-    mapk = f_mapk(map2k, N_k, params)
+    map3k = f_map3k(E1, N_3k, params, mode)
+    map2k = f_map2k(map3k, N_2k, params, mode)
+    mapk = f_mapk(map2k, N_k, params, mode)
 
     return {
         'map3k': map3k,
         'map2k': map2k,
         'mapk': mapk
     }
+
+
+def mapk_receiver(noise_dists):
+    # Parameters
+    mode = 'original'
+    al_m = torch.tensor(700.)
+    al_s = torch.tensor(1.)
+
+    nu_m = torch.tensor(.15)
+    nu_s = torch.tensor(.05)
+
+    params = {
+        "alpha_3k": Normal(al_m, al_s),
+        "alpha_2k": Normal(al_m, al_s),
+        "alpha_k": Normal(al_m, al_s),
+        "nu_3k": Normal(nu_m, nu_s),
+        "nu_2k": Normal(nu_m, nu_s),
+        "nu_k": Normal(nu_m, nu_s)
+    }
+
+    with pyro.iarange("model"):
+        E1 = Uniform(1.5e-5, 10.).rsample()
+        N_3k = sample('N_3k', noise_dists['N_3k'])
+        N_2k = sample('N_2k', noise_dists['N_2k'])
+        N_k = sample('N_k', noise_dists['N_k'])
+
+    map3k = f_map3k(E1, N_3k, params, mode)
+    map2k = f_map2k(map3k, N_2k, params, mode)
+    mapk = f_mapk(map2k, N_k, params, mode)
+
+    return {
+        'map3k': map3k,
+        'map2k': map2k,
+        'mapk': mapk
+    }
+
+
+def mapk_do_receiver(do_value,noise_dists):
+    mode = 'original'
+    # Parameters
+    al_m = torch.tensor(700.)
+    al_s = torch.tensor(1.)
+
+    nu_m = torch.tensor(.15)
+    nu_s = torch.tensor(.05)
+
+    params = {
+        "alpha_3k": Normal(al_m, al_s),
+        "alpha_2k": Normal(al_m, al_s),
+        "alpha_k": Normal(al_m, al_s),
+        "nu_3k": Normal(nu_m, nu_s),
+        "nu_2k": Normal(nu_m, nu_s),
+        "nu_k": Normal(nu_m, nu_s)
+    }
+
+    with pyro.iarange("model"):
+        E1 = Uniform(1.5e-5, 10.).rsample()
+        N_3k = sample('N_3k', noise_dists['N_3k'])
+        N_2k = sample('N_2k', noise_dists['N_2k'])
+        N_k = sample('N_k', noise_dists['N_k'])
+
+    map3k = do_value
+    map2k = f_map2k(map3k, N_2k, params, mode)
+    mapk = f_mapk(map2k, N_k, params, mode)
+
+    return map2k
